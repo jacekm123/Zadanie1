376023a1b3388e2df6c2399c3fd2282457073b5d
Jacek Mackowski <jacekm123@gmail.com>
2015-11-22 10:57:23 +0100

    jacek

---
+++ src/main.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class main {
+
+    public static void main(String[] args) {
+
+        FileReader fr = null;
+        String linia = "";
+
+        // OTWIERANIE PLIKU:
+        try {
+            fr = new FileReader("plik.txt");
+        } catch (FileNotFoundException e) {
+            System.out.println("BŁĄD PRZY OTWIERANIU PLIKU!");
+            System.exit(1);
+        }
+
+        BufferedReader bfr = new BufferedReader(fr);
+        // ODCZYT KOLEJNYCH LINII Z PLIKU:
+        try {
+            while((linia = bfr.readLine()) != null){
+                System.out.println(linia);
+            }
+        } catch (IOException e) {
+            System.out.println("BŁĄD ODCZYTU Z PLIKU!");
+            System.exit(2);
+        }
+
+        // ZAMYKANIE PLIKU
+        try {
+            fr.close();
+        } catch (IOException e) {
+            System.out.println("BŁĄD PRZY ZAMYKANIU PLIKU!");
+            System.exit(3);
+        }
+    }
+}
